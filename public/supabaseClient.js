b328339ad184e0fad149e310647028db3286d5b6
naaishaagarwal-wpi <nagarwal@wpi.edu>
2026-04-08 08:07:20 -0400

    changing supabase script

--- public/supabaseClient.js
+++ public/supabaseClient.js
@@ -2,7 +2,7 @@
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
-  );
+const supabaseUrl = "https://eucrnjfbbnpkhmslhlsx.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1Y3JuamZiYm5wa2htc2xobHN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NDczNDcsImV4cCI6MjA5MDAyMzM0N30.OZ4EJgTnKVcoxqnTuj4M8t27Ba_XU4Cbxv0bwRh7oiA";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
